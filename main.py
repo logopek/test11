31379559da17a99c5732faa1933e2e2c7ae2a987
logopek <49311518+logopek@users.noreply.github.com>
2024-02-07 22:45:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-from flask import Flask
-app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "hello world"
+import socket
 
+sock = socket.socket()
+sock.bind(('', 9090))
+sock.listen(1)
+conn, addr = sock.accept()
 
-app.run(port=56787)
+print 'connected:', addr
+
+while True:
+    data = conn.recv(1024)
+    if not data:
+        break
+    conn.send(data.upper())
+
+conn.close()
