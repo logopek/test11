aa9b2f6a3d81b800126729ccb0047d189e9bd0c7
logopek <49311518+logopek@users.noreply.github.com>
2024-02-08 22:29:39 +0300

    Update s.py

--- s.py
+++ s.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return """Настя лошара
-    <a display = "none" href="/love">Правильно мыслишь)</a>
+    <a display:none href="/love">Правильно мыслишь)</a>
     """
 
 @app.route("/love")
