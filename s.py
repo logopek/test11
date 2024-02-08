446942b51ca715de578c1397c3f77a59e5eb1778
logopek <49311518+logopek@users.noreply.github.com>
2024-02-08 11:15:30 +0300

    Update s.py

--- s.py
+++ s.py
@@ -4,15 +4,14 @@ app = Flask(__name__)
 text_to_return = "Hi!"
 @app.route("/")
 def hello_world():
-    s = "Hello!"
+    s = text_to_return
     response = make_response(s)
     response.headers.add('content-length', str(len(s.encode('utf-8'))))
     return response
 
 @app.route("/newMsg", methods = ["POST"])
 def newMsg():
-    print(request.data)
+    global text_to_return
+    x = json.loads(request.data)
+    text_to_return = x["text"]
     return "0"
-
-
-app.run(host="0.0.0.0",port = 8190)
