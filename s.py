7ee02b23cb49ff49c540226282d1dcf1873edc6c
logopek <49311518+logopek@users.noreply.github.com>
2024-02-08 22:21:30 +0300

    Update s.py

--- s.py
+++ s.py
@@ -1,20 +1,11 @@
-from flask import Flask, request, make_response
-import json
+from flask import Flask
 app = Flask(__name__)
-text_to_return = "Hi!"
+
 @app.route("/")
-def hello_world():
-    s = text_to_return
-    response = make_response(s)
-    response.headers.add('content-length', str(len(s.encode('utf-8'))))
-    return response
+def index():
+    return "Настя лошара"
 
-@app.route("/newMsg", methods = ["POST"])
-def newMsg():
-    global text_to_return
-    x = json.loads(request.data)
-    text_to_return = x["text"]
-    return "0"
-
-
+@app.route("/love")
+def love():
+    return "Конечно, это все шутки, Настя лучшая!"
 app.run(host="0.0.0.0",port = 8190)
